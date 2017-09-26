828f104538d293a5c5921c880f59836bcd945c7b
kevindmoore <kevindavmoore@gmail.com>
2017-09-25 20:16:39 -0700

    Cleanup DefaultViewHolder

--- app/src/main/java/com/raywenderlich/marsrovers/recyclerview/DefaultViewHolder.kt
+++ app/src/main/java/com/raywenderlich/marsrovers/recyclerview/DefaultViewHolder.kt
@@ -12,9 +12,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
-/**
- * A default view holder that makes it easy to find views
- */
 class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     protected var viewMap: MutableMap<Int, View> = HashMap()
 
@@ -22,33 +19,17 @@ class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         findViewItems(itemView)
     }
 
-    /**
-     * Set the string for the given textView id
-     * @param id
-     * @param text
-     */
     fun setText(@IdRes id: Int, text: String) {
         val view = (viewMap[id] ?: throw IllegalArgumentException("View for $id not found")) as? TextView ?: throw IllegalArgumentException("View for $id is not a TextView")
         view.text = text
     }
 
-    /**
-     * Set the string for the given textView id
-     * @param id
-     * @param textRes
-     */
     fun setText(@IdRes id: Int, @StringRes textRes: Int) {
         val view = (viewMap[id] ?: throw IllegalArgumentException("View for $id not found")) as? TextView ?: throw IllegalArgumentException("View for $id is not a TextView")
         view.setText(textRes)
     }
 
 
-    /**
-     * Set the ImageView with the given id
-     * @param id
-     * @param drawable
-     * @return ImageView
-     */
     fun setImage(@IdRes id: Int, @DrawableRes drawable: Int) {
         val view = viewMap[id]
         when (viewMap[id]) {
@@ -61,12 +42,6 @@ class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         }
     }
 
-    /**
-     * Set the ImageView with the given id
-     * @param id
-     * @param drawable
-     * @return ImageView
-     */
     fun setImage(@IdRes id: Int, drawable: Drawable) {
         val view = viewMap[id]
         when (viewMap[id]) {
@@ -80,20 +55,11 @@ class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     }
 
 
-    /**
-     * Get the ImageView with the given id
-     * @param id
-     * @return ImageView
-     */
     fun getImage(@IdRes id: Int): ImageView {
         val view = (viewMap[id] ?: throw IllegalArgumentException("View for $id not found")) as? ImageView ?: throw IllegalArgumentException("View for $id is not a ImageView")
         return view
     }
 
-    /**
-     * Find all views and put them in a map
-     * @param itemView
-     */
     protected fun findViewItems(itemView: View) {
         if (itemView is ViewGroup) {
             if (itemView.getId() != View.NO_ID) {
@@ -101,10 +67,9 @@ class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             } else {
                 viewMap.put(View.generateViewId(), itemView)
             }
-            val viewGroup = itemView
-            val childCount = viewGroup.childCount
-            for (i in 0..childCount - 1) {
-                val childView = viewGroup.getChildAt(i)
+          val childCount = itemView.childCount
+            for (i in 0 until childCount) {
+                val childView = itemView.getChildAt(i)
                 if (childView.id != View.NO_ID) {
                     viewMap.put(childView.id, childView)
                 } else {
