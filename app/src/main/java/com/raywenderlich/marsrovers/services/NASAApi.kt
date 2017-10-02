d558840fea49337b9910bdfea8a60344fd8f531e
kevindmoore <kevindavmoore@gmail.com>
2017-10-01 20:08:11 -0700

    Remove key

--- app/src/main/java/com/raywenderlich/marsrovers/services/NASAApi.kt
+++ app/src/main/java/com/raywenderlich/marsrovers/services/NASAApi.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Path
 
 
 interface NASAApi {
-  @GET("mars-photos/api/v1/rovers/{rover}/photos?sol=1000&api_key=<your key here>")
+  @GET("mars-photos/api/v1/rovers/{rover}/photos?sol=1000&api_key=MfrPknQajnh5yQI5ozcUkORXjfGYJIDKCirp6DKa")
   fun getPhotos(@Path("rover") rover: String): Call<PhotoList>
 }
